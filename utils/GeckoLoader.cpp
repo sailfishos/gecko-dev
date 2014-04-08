deef54c7cb82c09bbb6e2d67e7557da1908977b2
Raine Makelainen <raine.makelainen@jollamobile.com>
2014-04-08 18:43:40 +0300

    Fix booster startup

--- utils/GeckoLoader.cpp
+++ utils/GeckoLoader.cpp
@@ -107,7 +107,7 @@ GeckoLoader::InitEmbedding(const char* aProfilePath)
   }
 
   // create nsIFile pointing to appdir
-  char self[MAX_PATH];
+  char self[MAX_PATH] = "";
 #ifdef WIN32
   GetModuleFileNameA(GetModuleHandle(NULL), self, sizeof(self));
 #else
@@ -121,10 +121,15 @@ GeckoLoader::InitEmbedding(const char* aProfilePath)
   size_t lastslash_t = selfPath.find_last_of("/\\");
   if (lastslash_t == std::string::npos) {
     LOGE("Invalid module filename: %s", self);
+#ifdef WIN32
     return false;
+#else
+    // A hackish way to get this initialized when starting through booster.
+    selfPath = std::string("/usr/bin");
+#endif
+  } else {
+    selfPath = selfPath.substr(0, lastslash_t);
   }
-
-  selfPath = selfPath.substr(0, lastslash_t);
 
   nsCOMPtr<nsIFile> appdir;
   rv = XRE_GetBinaryPath(selfPath.c_str(), getter_AddRefs(appdir));
