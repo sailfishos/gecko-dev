8078a05a5fc8d35f611fd677b60a20e614ec5a57
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2014-08-12 17:51:19 +0300

    Enable APZ

--- embedding/embedlite/embedding.js
+++ embedding/embedlite/embedding.js
@@ -43,6 +43,7 @@ pref("browser.dom.window.dump.enabled", true);
 pref("layers.acceleration.draw-fps", false);
 pref("layers.use-deprecated-textures", false);
 pref("layers.enable-tiles", true);
+pref("layers.async-pan-zoom.enabled", true);
 pref("font.size.inflation.disabledInMasterProcess", true);
 pref("apz.asyncscroll.throttle", 15);
 pref("apz.y_skate_size_multiplier", "4.5f");
