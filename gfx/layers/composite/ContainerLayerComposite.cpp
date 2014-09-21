953c7b8e6b8c50d0c70d7bcff74823887d4982b0
Tatiana Meshkova <tanya.meshkova@gmail.com>
2014-09-21 16:03:40 -0700

    Fixed embedded rendering in Gecko 34

--- gfx/layers/composite/ContainerLayerComposite.cpp
+++ gfx/layers/composite/ContainerLayerComposite.cpp
@@ -201,18 +201,6 @@ ContainerPrepare(ContainerT* aContainer,
       continue;
     }
 
-    RenderTargetRect quad = layerToRender->GetLayer()->
-      TransformRectToRenderTarget(LayerPixel::FromUntyped(
-        layerToRender->GetLayer()->GetEffectiveVisibleRegion().GetBounds()));
-
-    Compositor* compositor = aManager->GetCompositor();
-    if (!layerToRender->GetLayer()->AsContainerLayer() &&
-        !quad.Intersects(compositor->ClipRectInLayersCoordinates(layerToRender->GetLayer(), clipRect)) &&
-        !LayerHasCheckerboardingAPZC(layerToRender->GetLayer(), nullptr)) {
-      CULLING_LOG("Sublayer %p is clipped entirely\n", layerToRender->GetLayer());
-      continue;
-    }
-
     CULLING_LOG("Preparing sublayer %p\n", layerToRender->GetLayer());
 
     nsIntRegion savedVisibleRegion;
@@ -309,7 +297,7 @@ RenderLayers(ContainerT* aContainer,
         gfx::Rect clipRect(aClipRect.x, aClipRect.y, aClipRect.width, aClipRect.height);
         compositor->DrawQuad(
           RenderTargetPixel::ToUnknown(
-            compositor->ClipRectInLayersCoordinates(aContainer, aClipRect)),
+            compositor->ClipRectInLayersCoordinates(aClipRect)),
           clipRect, effectChain, opacity, Matrix4x4());
       }
     }
@@ -437,6 +425,7 @@ RenderIntermediate(ContainerT* aContainer,
   if (!surface) {
     return;
   }
+
   compositor->SetRenderTarget(surface);
   // pre-render all of the layers into our temporary
   RenderLayers(aContainer, aManager, RenderTargetPixel::FromUntyped(aClipRect));
