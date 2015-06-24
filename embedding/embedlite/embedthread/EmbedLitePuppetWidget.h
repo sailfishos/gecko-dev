3c919a591866554545106252735f314616fed9b1
Piotr Tworek <piotr.tworek@jollamobile.com>
2015-06-24 15:13:14 +0200

    [embedlite] Add support for creating external GL context early. JB#30162
    In sailfish browser we're interested in creating external window
    compositing surface as early as possible. To avoid seeing any rendering
    artifacts / content from a previously rendered web page we want to reset
    the state of the surface to a know state. Unfortunately gecko creates
    it's compositor context after the page being loaded requests it's first
    frame to be painted. In majority of cases this is to late for us.
    
    This patch introduces an additional gecko pref which asks the embedder
    to create it's GL context as soon as top level PuppetWidget is created
    for the view. The initial request is made from the compositor thread
    since the GLContext object created by the application will be used from
    this thread. The PuppetWidget does not do any caching of the requested
    context. It's expected that this part will be handled by the application
    itself (already the case for sailfish-browser).

--- embedding/embedlite/embedthread/EmbedLitePuppetWidget.h
+++ embedding/embedlite/embedthread/EmbedLitePuppetWidget.h
@@ -181,6 +181,7 @@ private:
   nsresult Paint();
   bool ViewIsValid();
   mozilla::gl::GLContext* GetGLContext() const;
+  static void CreateGLContextEarly(uint32_t aViewId);
 
   EmbedLitePuppetWidget* TopWindow();
   bool IsTopLevel();
