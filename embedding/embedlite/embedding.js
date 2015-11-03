b3b67870d6f26e1a22cd80ff2f269b581aed3f40
Raine Makelainen <raine.makelainen@jolla.com>
2015-11-03 14:42:19 +0200

    [pref] Merge "optimize images memory usage" from embedlite_31

--- embedding/embedlite/embedding.js
+++ embedding/embedlite/embedding.js
@@ -344,6 +344,7 @@ pref("media.video-queue.default-size", 3);
 // optimize images memory usage
 pref("image.mem.decodeondraw", true);
 pref("image.mem.allow_locking_in_content_processes", false);
+pref("image.mem.min_discard_timeout_ms", 10000);
 pref("image.onload.decode.limit", 24); /* don't decode more than 24 images eagerly */
 
 // SimplePush
