86a59a26b0ec3edc142e2773aa1e4bfa960657ce
Raine Makelainen <raine.makelainen@jolla.com>
2020-02-28 11:38:58 +0200

    Revert "Bug 1282866 - remove widget/qt and other supporting QT code, r=dougt.  This patch does not remove all of the checks for MOZ_WIDGET_QT (which are dead code), but that will be a followup mentored bug."
    This reverts commit bf05496a833cfc1b4fb168b94e75fc93086111f2.
    
    This does not revert back pango configuration as it seems to be already removed.

--- dom/geolocation/nsGeolocation.cpp
+++ dom/geolocation/nsGeolocation.cpp
@@ -29,6 +29,10 @@
 
 class nsIPrincipal;
 
+#ifdef MOZ_ENABLE_QT5GEOPOSITION
+#include "QTMLocationProvider.h"
+#endif
+
 #ifdef MOZ_WIDGET_ANDROID
 #include "AndroidLocationProvider.h"
 #endif
@@ -683,6 +687,10 @@ nsresult nsGeolocationService::Init()
   }
 
   obs->AddObserver(this, "xpcom-shutdown", false);
+
+#ifdef MOZ_ENABLE_QT5GEOPOSITION
+  mProvider = new QTMLocationProvider();
+#endif
 
 #ifdef MOZ_WIDGET_ANDROID
   mProvider = new AndroidLocationProvider();
