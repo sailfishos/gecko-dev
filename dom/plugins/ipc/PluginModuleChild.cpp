a88ec965fe48fc2ef2f05c517fec58dddc62f87d
Raine Makelainen <raine.makelainen@jolla.com>
2020-03-17 12:28:38 +0200

    Revert "Bug 1285554 - Remove remnants of widget/qt. r=dougt,mshal."
    This reverts commit a913f991077467a246072b3a8ecb45838a1363ba.

--- dom/plugins/ipc/PluginModuleChild.cpp
+++ dom/plugins/ipc/PluginModuleChild.cpp
@@ -334,7 +334,6 @@ PluginModuleChild::InitForChrome(const std::string& aPluginFilename,
 }
 
 #if defined(MOZ_WIDGET_GTK)
-
 typedef void (*GObjectDisposeFn)(GObject*);
 typedef gboolean (*GtkWidgetScrollEventFn)(GtkWidget*, GdkEventScroll*);
 typedef void (*GtkPlugEmbeddedFn)(GtkPlug*);
