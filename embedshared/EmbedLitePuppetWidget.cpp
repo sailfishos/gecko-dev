389f3362a60d2ecd366822efb0e864f168a98cfe
Raine Makelainen <raine.makelainen@jolla.com>
2020-05-25 16:55:24 +0300

    [sailfishos][embedlite] Share layermanager correctly. Fixes JB#49914
    This commit also guards few please use after destroyed.
    
    Signed-off-by: Raine Makelainen <raine.makelainen@jolla.com>

--- embedshared/EmbedLitePuppetWidget.cpp
+++ embedshared/EmbedLitePuppetWidget.cpp
@@ -387,11 +387,12 @@ void EmbedLitePuppetWidget::CreateCompositor(int aWidth, int aHeight)
 LayerManager *
 EmbedLitePuppetWidget::GetLayerManager(PLayerTransactionChild *aShadowManager, LayersBackend aBackendHint, LayerManagerPersistence aPersistence)
 {
-  LOGT();
+  LOGC("EmbedLiteLayerManager", "layer manager %p is shutdown %p this: %p", mLayerManager, mShutdownObserver, this);
 
   if (!mLayerManager) {
-    if (!mShutdownObserver) {
+    if (!mShutdownObserver || mOnDestroyCalled) {
       // We are shutting down, do not try to re-create a LayerManager
+      LOGC("EmbedLiteLayerManager", "Shutting down or puppet destroyed.");
       return nullptr;
     }
   }
@@ -399,6 +400,7 @@ EmbedLitePuppetWidget::GetLayerManager(PLayerTransactionChild *aShadowManager, L
   LayerManager *lm = PuppetWidgetBase::GetLayerManager(aShadowManager, aBackendHint, aPersistence);
   if (lm) {
     mLayerManager = lm;
+    LOGC("EmbedLiteLayerManager", "base layer manager: %p this: %p", mLayerManager, this);
     return mLayerManager;
   }
 
@@ -413,7 +415,9 @@ EmbedLitePuppetWidget::GetLayerManager(PLayerTransactionChild *aShadowManager, L
     return mLayerManager;
   }
 
-  mLayerManager = new ClientLayerManager(this);
+  nsIWidget* topWidget = GetTopLevelWidget();
+  mLayerManager = topWidget->GetLayerManager(aShadowManager, aBackendHint, aPersistence);
+  LOGC("EmbedLiteLayerManager", "Get layer manager from top: %p this: %p", mLayerManager, this);
   return mLayerManager;
 }
 
