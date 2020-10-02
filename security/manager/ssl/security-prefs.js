dade4d57225add3562b0d1e42285c69de6d221ed
David Llewellyn-Jones <david.llewellyn-jones@jolla.com>
2020-10-02 12:57:02 +0000

    [sailfishos][gecko] Disable TLS 1.0 and 1.1. Fixes JB#50522
    Increases the minimum supported TLS version from 1.0 to 1.2, independent
    of user preferences.
    
    This follows MB#1643229 introduced in FF78. See
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1643229
    https://phabricator.services.mozilla.com/D78215

--- security/manager/ssl/security-prefs.js
+++ security/manager/ssl/security-prefs.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("security.tls.version.min", 1);
+pref("security.tls.version.min", 3);
 pref("security.tls.version.max", 4);
 pref("security.tls.version.fallback-limit", 3);
 pref("security.tls.insecure_fallback_hosts", "");
