60bcbcb7f498d40bf462351ca24fcbf530d6f033
Raine Makelainen <raine.makelainen@jolla.com>
2021-09-07 19:17:56 +0300

    [sailfishos][contentcontrollre] Use MessageLoop to forward events. JB#55326 OMP#JOLLA-327
    The current nsThread passed by the EmbedLiteViewParent does not
    have event queue initialized i.e. it was initialized with nsThread()
    constructor. This initialization step looks to be coming via
    nsThreadManager::GetCurrentThread.
    
    The MessageLoop has nsISerialEventTarget in place. Thus, it can be
    used to forward events as we did in past.
    
    This commit effectively reverts sha1 c5d20afd1dc6cee148d6f6b2a10f371558f72ef6

--- embedding/embedlite/embedthread/EmbedContentController.h
+++ embedding/embedlite/embedthread/EmbedContentController.h
@@ -71,10 +71,12 @@ private:
   void HandleSingleTap(const LayoutDevicePoint aPoint, Modifiers aModifiers, const ScrollableLayerGuid aGuid, uint64_t aInputBlockId);
   void HandleLongTap(const LayoutDevicePoint aPoint, Modifiers aModifiers, const ScrollableLayerGuid aGuid, uint64_t aInputBlockId);
 
+  void DoRequestContentRepaint(const layers::RepaintRequest aRequest);
   void DoSendScrollEvent(const layers::RepaintRequest aRequest);
 
   nsIntPoint convertIntPoint(const LayoutDevicePoint &aPoint);
 
+  MessageLoop* mUILoop;
   nsISerialEventTarget *mUIThread;
   EmbedLiteViewParent* mRenderFrame;
 };
