a09417a0577f315d7cf0ab1eec553f4b950fa12a
Andrew Branson <andrew.branson@jolla.com>
2026-07-25 14:04:21 +0200

    [sailfishos][media] Disable RDD for EmbedLite. Fixes JB#64794
    EmbedLite does not provide Gecko's remote decoder path. Disable the RDD
    process so supported local decoders, including dav1d, can be selected.

--- embedding/embedlite/embedding.js
+++ embedding/embedlite/embedding.js
@@ -374,6 +374,8 @@ pref("media.cache_readahead_limit", 30);
 pref("media.video-queue.default-size", 3);
 
 // Use Gecko/FFmpeg/gecko-camera media paths; gmp-droid is no longer shipped.
+// EmbedLite has no remote RDD decoder path; decode supported formats locally.
+pref("media.rdd-process.enabled", false);
 pref("media.gmp.decoder.enabled", false);
 pref("media.decoder.recycle.enabled", true);
 
